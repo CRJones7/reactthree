cfe55815cf723c5d032a08d9265275d72af0ede6
Courtney <courtneyrjones95@gmail.com>
2022-12-26 13:51:04 -0600

    ground

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, {Suspense} from 'react';
 import {Canvas} from "@react-three/fiber";
 import "./style.css"
 import { OrbitControls, PerspectiveCamera } from '@react-three/drei';
-
+import Ground from "./Ground"
 
 function CarShow(){
   return (
@@ -12,10 +12,39 @@ function CarShow(){
     {/* camera itself field of view and the default */}
     <PerspectiveCamera makeDefault fov={50} position={[3,2,5]}/>
 
-    <mesh>
+    {/* let color = new Color(0,0,0); */}
+    <color args={[0,0,0]} attach="background"/>
+
+    {/* let spotlight = new Spotlight();
+    spotlight.intensity = 1.5;
+    spotlight.position.set(...)
+     */}
+
+     <spotLight
+      color={[1, 0.25, 0.7]}
+      intensity={1.5}
+      angle={0.6}
+      penumbra={0.5}
+      position={[5,5,0]}
+      castShadow
+      shadow-bias={-0.0001}
+     />
+
+     <spotLight
+     color={[0.14, 0.5, 1]}
+     intensity={2}
+     angle={0.6}
+     penumbra={0.5}
+     postion={[-5, 5, 0]}
+     castShadow
+     shadow-bias={-0.0001}
+     />
+
+    {/* <mesh>
       <boxGeometry args={[1,1,1]}/>
       <meshBasicMaterial color={'green'}/>
-    </mesh>
+    </mesh> */}
+    <Ground/>
     </>
   )
 }
