d309f6f624988ae6d88d29ffcc86974d10564640
Courtney <courtneyrjones95@gmail.com>
2022-12-26 13:17:22 -0600

    initial setup

--- src/App.js
+++ src/App.js
@@ -1,18 +1,33 @@
-import React from 'react';
+import React, {Suspense} from 'react';
+import {Canvas} from "@react-three/fiber";
+import "./style.css"
+import { OrbitControls, PerspectiveCamera } from '@react-three/drei';
 
-function App() {
+
+function CarShow(){
   return (
-    <div style={{ textAlign: 'center' }}>
-      <header>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+    {/* moves camera */}
+    <OrbitControls target={[0, 0.35, 0]} maxPolarAngle={1.45}/>
+    {/* camera itself field of view and the default */}
+    <PerspectiveCamera makeDefault fov={50} position={[3,2,5]}/>
+
+    <mesh>
+      <boxGeometry args={[1,1,1]}/>
+      <meshBasicMaterial color={'green'}/>
+    </mesh>
+    </>
+  )
+}
+
+function App(){
+  return(
+    <Suspense fallback={null}>
+      <Canvas shadows>
+        <CarShow/>
+      </Canvas>
+    </Suspense>
+  )
 }
 
 export default App;
